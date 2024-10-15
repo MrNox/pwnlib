496fcb27a577be99598f896ca4959dacdb4a9472
nox <noxh4x@gmail.com>
2024-10-14 21:06:48 -0300

    bytes2str added

--- pwnlib/binary/encoding/byte.py
+++ pwnlib/binary/encoding/byte.py
@@ -8,3 +8,11 @@ def str2bytes(data: str) -> bytes:
         return data
     else:
         raise ValueError("{} given ('str' expected)".format(type(data)))
+
+def bytes2str(data: bytes) -> str:
+    if isinstance(data, bytes):
+        return ''.join(list(map(chr, data)))
+    elif isinstance(data, str):
+        return data
+    else:
+        raise ValueError("{} given ('bytes' expected)".format(type(data)))
