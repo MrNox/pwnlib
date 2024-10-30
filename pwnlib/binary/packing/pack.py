f947898df060f1f25afba6132a4cd61a4d84cfdd
nox <noxh4x@gmail.com>
2024-10-30 13:38:01 -0300

    byteorder initialized

--- pwnlib/binary/packing/pack.py
+++ pwnlib/binary/packing/pack.py
@@ -6,13 +6,13 @@ def p8(data:int) -> bytes:
     return (data & 0xFF).to_bytes(1, byteorder='little')
 
 
-def p16(data, byteorder) -> bytes:
+def p16(data, byteorder:str='little') -> bytes:
     assert isinstance(data, int), "{} given, must be 'int')".format(type(data))
 
     return (data & 0xFFFF).to_bytes(2, byteorder=byteorder)
 
 
-def p32(data, byteorder) -> bytes:
+def p32(data, byteorder:str='little') -> bytes:
     assert isinstance(data, (int, float)), "{} given, must be 'int' or 'float')".format(type(data))
 
 
@@ -25,7 +25,7 @@ def p32(data, byteorder) -> bytes:
     return (data & 0xFFFFFFFF).to_bytes(4, byteorder=byteorder)
 
 
-def p64(data, byteorder) -> bytes:
+def p64(data, byteorder:str='little') -> bytes:
     assert isinstance(data, (int, float)), "{} given, must be 'int' or 'double'".format(type(data))
 
     if isinstance(data, float):
